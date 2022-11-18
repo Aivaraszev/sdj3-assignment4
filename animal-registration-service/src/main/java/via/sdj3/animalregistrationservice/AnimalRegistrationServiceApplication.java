68c97c7170d2f626aa6287256a3c6c80f59aec07
M0Rek <morek.090@gmail.com>
2022-11-18 11:57:22 +0100

    Add setup for h2 db in animal registration service.

--- animal-registration-service/src/main/java/via/sdj3/animalregistrationservice/AnimalRegistrationServiceApplication.java
+++ animal-registration-service/src/main/java/via/sdj3/animalregistrationservice/AnimalRegistrationServiceApplication.java
@@ -1,13 +1,22 @@
 package via.sdj3.animalregistrationservice;
 
+import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class AnimalRegistrationServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AnimalRegistrationServiceApplication.class, args);
+    }
+
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public Server h2Server() throws SQLException {
+        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092");
     }
 
 }
